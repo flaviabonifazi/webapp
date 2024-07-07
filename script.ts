c1eeff5bcf118101545d4d3fdf7e22dc0400ef48
flaviabonifazi <163485688+flaviabonifazi@users.noreply.github.com>
2024-07-08 00:29:20 +0200

    Update script.ts

--- script.ts
+++ script.ts
@@ -1,8 +1,1 @@
-// Seleziona l'icona del menu e il menu laterale
-const menuIcon = document.querySelector('.menu-icon') as HTMLElement;
-const sidebar = document.querySelector('.sidebar') as HTMLElement;
 
-// Aggiungi un gestore per il click sull'icona del menu
-menuIcon.addEventListener('click', () => {
-    sidebar.classList.toggle('active');
-});
